0dabac2f5473a33d00060752ebb76fe8656eb8ea
CorreaJose13 <correa.jose@correounivalle.edu.co>
2025-01-20 22:00:45 -0500

    restyle front end and implement initial approach for modals

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('Happy developing ✨')
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+
+const closeModal = function () {
+    modal.classList.add("hidden");
+    overlay.classList.add("hidden");
+};
+
+
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", function (e) {
+    if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+        closeModal();
+    }
+});
+
+const openModal = function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+};
