6a4c97441515fa618ed8b65906e6f049150bc442
v1nay3010 <angie_van.fajardo@uao.edu.co>
2025-01-22 03:57:04 -0500

    getAllBooks, getAllAuthors done

--- api/routes/rauthor.js
+++ api/routes/rauthor.js
@@ -7,6 +7,7 @@ var api = express.Router();
 
 api.post('/saveAuthor', AuthorController.saveAuthor);
 api.get('/author/:name', AuthorController.getAuthor);
+api.get('/authors', AuthorController.getAuthors);
 api.put('/updateAuthor/:name', AuthorController.updateAuthor);
 api.delete('/deleteAuthor/:name', AuthorController.deleteAuthor);
 
--- api/routes/rbook.js
+++ api/routes/rbook.js
@@ -7,6 +7,7 @@ var api = express.Router();
 
 api.post('/saveBook', BookController.saveBook);
 api.get('/book/:isbn', BookController.getBook);
+api.get('/books/', BookController.getBooks);
 api.put('/updateBook/:isbn', BookController.updateBook);
 api.delete('/deleteBook/:isbn', BookController.deleteBook);
 
